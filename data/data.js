25e1ecaada474e6f688d2fd071274ffe2b80e5a0
yawbtng <154343001+yawbtng@users.noreply.github.com>
2025-06-05 14:21:08 -0700

    Attempting to configure dynamic modal implementation

--- data/data.js
+++ data/data.js
@@ -1,95 +1,258 @@
-plyalists =  [
-    {
-      "playlistID": 1,
-      "playlist_name": "Summer Vibes",
-      "playlist_author": "John Doe",
-      "playlist_art": "assets/img/playlist.png",
-      "songs": [
-        {
-          "songID": 1,
-          "song_name": "Happy",
-          "artist": "Pharrell Williams"
-        },
-        {
-          "songID": 2,
-          "song_name": "Uptown Funk",
-          "artist": "Mark Ronson ft. Bruno Mars"
-        },
-        {
-          "songID": 3,
-          "song_name": "Can't Stop the Feeling!",
-          "artist": "Justin Timberlake"
-        }
-      ]
-    },
-    {
-      "playlistID": 2,
-      "playlist_name": "Workout Mix",
-      "playlist_author": "Jane Smith",
-      "playlist_art": "assets/img/playlist.png",
-      "songs": [
-        {
-          "songID": 4,
-          "song_name": "Eye of the Tiger",
-          "artist": "Survivor"
-        },
-        {
-          "songID": 5,
-          "song_name": "Lose Yourself",
-          "artist": "Eminem"
-        },
-        {
-          "songID": 6,
-          "song_name": "We Will Rock You",
-          "artist": "Queen"
-        }
-      ]
-    },
-    {
-      "playlistID": 3,
-      "playlist_name": "Relaxing Music",
-      "playlist_author": "Bob Johnson",
-      "playlist_art": "assets/img/playlist.png",
-      "songs": [
-        {
-          "songID": 7,
-          "song_name": "Weightless",
-          "artist": "Marconi Union"
-        },
-        {
-          "songID": 8,
-          "song_name": "Clair de Lune",
-          "artist": "Claude Debussy"
-        },
-        {
-          "songID": 9,
-          "song_name": "River Flows in You",
-          "artist": "Yiruma"
-        }
-      ]
-    },
-
-    {
-      "playlistID": 4,
-      "playlist_name": "Jazz Classics",
-      "playlist_author": "Emily Chen",
-      "playlist_art": "assets/img/playlist.png",
-      "songs": [
-        {
-          "songID": 10,
-          "song_name": "Moon River",
-          "artist": "Frank Sinatra"
-        },
-        {
-          "songID": 11,
-          "song_name": "My Funny Valentine",
-          "artist": "Chet Baker"
-        },
-        {
-          "songID": 12,
-          "song_name": "The Girl from Ipanema",
-          "artist": "Stan Getz and Astrud Gilberto"
-        }
-      ]
-    }
-  ]
+playlists = [
+  {
+    "playlistID": 1,
+    "playlist_name": "Summer Vibes",
+    "playlist_author": "John Doe",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 1,
+        "song_name": "Happy",
+        "artist": "Pharrell Williams",
+        "album": "G I R L",
+        "duration": "3:53",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 2,
+        "song_name": "Uptown Funk",
+        "artist": "Mark Ronson ft. Bruno Mars",
+        "album": "Uptown Special",
+        "duration": "4:38",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 3,
+        "song_name": "Can't Stop the Feeling!",
+        "artist": "Justin Timberlake",
+        "album": "Trolls: Original Motion Picture Soundtrack",
+        "duration": "3:56",
+        "image": "assets/img/song.png"
+      }
+    ]
+  },
+  {
+    "playlistID": 2,
+    "playlist_name": "Workout Mix",
+    "playlist_author": "Jane Smith",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 4,
+        "song_name": "Eye of the Tiger",
+        "artist": "Survivor",
+        "album": "Eye of the Tiger",
+        "duration": "3:59",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 5,
+        "song_name": "Lose Yourself",
+        "artist": "Eminem",
+        "album": "8 Mile: Music from and Inspired by the Motion Picture",
+        "duration": "5:26",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 6,
+        "song_name": "We Will Rock You",
+        "artist": "Queen",
+        "album": "News of the World",
+        "duration": "2:28",
+        "image": "assets/img/song.png"
+      }
+    ]
+  },
+  {
+    "playlistID": 3,
+    "playlist_name": "Relaxing Music",
+    "playlist_author": "Bob Johnson",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 7,
+        "song_name": "Weightless",
+        "artist": "Marconi Union",
+        "album": "Gravity",
+        "duration": "8:45",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 8,
+        "song_name": "Clair de Lune",
+        "artist": "Claude Debussy",
+        "album": "Suite Bergamasque",
+        "duration": "5:00",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 9,
+        "song_name": "River Flows in You",
+        "artist": "Yiruma",
+        "album": "First Love",
+        "duration": "3:38",
+        "image": "assets/img/song.png"
+      }
+    ]
+  },
+  {
+    "playlistID": 4,
+    "playlist_name": "Jazz Classics",
+    "playlist_author": "Emily Chen",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 10,
+        "song_name": "Moon River",
+        "artist": "Frank Sinatra",
+        "album": "Moon River",
+        "duration": "2:43",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 11,
+        "song_name": "My Funny Valentine",
+        "artist": "Chet Baker",
+        "album": "Chet Baker Sings",
+        "duration": "2:19",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 12,
+        "song_name": "The Girl from Ipanema",
+        "artist": "Stan Getz and Astrud Gilberto",
+        "album": "Getz/Gilberto",
+        "duration": "5:24",
+        "image": "assets/img/song.png"
+      }
+    ]
+  },
+  {
+    "playlistID": 5,
+    "playlist_name": "Rock Anthems",
+    "playlist_author": "David Lee",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 13,
+        "song_name": "Sweet Child O' Mine",
+        "artist": "Guns N' Roses",
+        "album": "Appetite for Destruction",
+        "duration": "4:58",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 14,
+        "song_name": "Bohemian Rhapsody",
+        "artist": "Queen",
+        "album": "A Night at the Opera",
+        "duration": "5:55",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 15,
+        "song_name": "Hotel California",
+        "artist": "Eagles",
+        "album": "Hotel California",
+        "duration": "6:30",
+        "image": "assets/img/song.png"
+      }
+    ]
+  },
+  {
+    "playlistID": 6,
+    "playlist_name": "Hip-Hop Favorites",
+    "playlist_author": "Sarah Taylor",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 16,
+        "song_name": "Lose Yourself",
+        "artist": "Eminem",
+        "album": "8 Mile: Music from and Inspired by the Motion Picture",
+        "duration": "5:26",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 17,
+        "song_name": "Juicy",
+        "artist": "The Notorious B.I.G.",
+        "album": "Ready to Die",
+        "duration": "5:27",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 18,
+        "song_name": "Forgot About Dre",
+        "artist": "Dr. Dre ft. Eminem",
+        "album": "2001",
+        "duration": "3:42",
+        "image": "assets/img/song.png"
+      }
+    ]
+  },
+  {
+    "playlistID": 7,
+    "playlist_name": "Pop Hits",
+    "playlist_author": "Emily Chen",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 19,
+        "song_name": "Uptown Funk",
+        "artist": "Mark Ronson ft. Bruno Mars",
+        "album": "Uptown Special",
+        "duration": "4:38",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 20,
+        "song_name": "Can't Stop the Feeling!",
+        "artist": "Justin Timberlake",
+        "album": "Trolls: Original Motion Picture Soundtrack",
+        "duration": "3:56",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 21,
+        "song_name": "Happy",
+        "artist": "Pharrell Williams",
+        "album": "Despicable Me 2: Original Motion Picture Soundtrack",
+        "duration": "3:53",
+        "image": "assets/img/song.png"
+      }
+    ]
+  },
+  {
+    "playlistID": 8,
+    "playlist_name": "Electronic Dance Music",
+    "playlist_author": "David Lee",
+    "playlist_art": "assets/img/playlist.png",
+    "songs": [
+      {
+        "songID": 22,
+        "song_name": "Sandstorm",
+        "artist": "Darude",
+        "album": "Before the Storm",
+        "duration": "3:46",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 23,
+        "song_name": "One",
+        "artist": "Veracocha",
+        "album": "Carte Blanche",
+        "duration": "5:04",
+        "image": "assets/img/song.png"
+      },
+      {
+        "songID": 24,
+        "song_name": "Children",
+        "artist": "Robert Miles",
+        "album": "Dreamland",
+        "duration": "7:06",
+        "image": "assets/img/song.png"
+      }
+    ]
+  }
+]
--- script.js
+++ script.js
@@ -1,11 +1,75 @@
-const modal = document.getElementById("playlistModal");
-const span = document.getElementsByClassName("close")[0];
+const loadCards = () => {
+   console.log("loading playlist cards and playlist modals");
+   const playlistCards = document.getElementById("playlist-cards");
+   const playlistModals = document.getElementById("playlistModal");
 
-function openModal() {
-   modal.style.display = "block";
+   for (const list of playlists) {
+         const el = createPlaylist(list)
+         playlistCards.appendChild(el)
+
+         const modal = createPlaylistModal(list)
+         playlistModals.appendChild(modal);
+   }
 }
 
-span.onclick = function() {
-   modal.style.display = "none";
+document.addEventListener("DOMContentLoaded", () => {
+   loadCardsAndModals();
+})
+
+
+
+
+const createPlaylist = (list) => {
+      console.log(list);
+      const playlistElement = document.createElement("div");
+      playlistElement.className = "p-card"
+
+      playlistElement.innerHTML = `
+                  <img src="${list.playlist_art}">
+                  <h4 class="playlist-title">${list.playlist_name}</h4>
+                  <h6 class="playlist-author">${list.playlist_author}</h6>
+                  <button class="playlist-like">Like ♡</button>
+               `
+      
+      const modal = createPlaylistModal(list);
+      
+      return playlistElement;
 }
 
+const createPlaylistModal = (modal) => {
+   console.log(modal);
+   const modalElement = document.querySelector(".modal");
+   let songList = "";
+
+   modal.songs.forEach((song) => {
+      console.log(song)
+      songList += `
+         <div class="song">
+            <img src="${song.image}">
+            <div class="song-text">
+               <h4>${song.song_name}</h4>
+               <p>${song.artist}</p>
+               <p>${song.album}</p>
+            </div>
+            <h6>${song.duration}</h6>
+         </div>
+      `
+   })
+
+   modalElement.innerHTML = `
+      <div class="modal-content">
+         <span class="close" >&times;</span>
+
+         <div class="modal-playlist">
+            <img class="playlistImage" src="${modal.playlist_art}">
+            <div>
+               <h1 class="palylistName">${modal.playlist_name}</h1>
+               <p class="creatorName">${modal.playlist_author}</p>
+            </div>
+         </div>
+         ${songList}
+      </div>
+   `
+
+   return modalElement;
+}
